7319b9616e7c6522e7c06a814139351e51115b28
yangjie76 <yangjie76@meituan.com>
2022-05-05 21:08:41 +0800

    add singleton

---
+++ other/singleton/call_once_singleton.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <memory>
+#include <mutex>
+
+class Singleton {
+public:
+  static Singleton& GetInstance() {
+    static std::once_flag s_flag;
+    std::call_once(s_flag, [&]() {
+      instance_.reset(new Singleton);
+    });
+
+    return *instance_;
+  }
+
+  ~Singleton() = default;
+
+  void PrintAddress() const {
+    std::cout << this << std::endl;
+  }
+
+private:
+  Singleton() = default;
+
+  Singleton(const Singleton&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+
+private:
+  static std::unique_ptr<Singleton> instance_;
+};
+
+std::unique_ptr<Singleton> Singleton::instance_;
+
+int main() {
+  Singleton& s1 = Singleton::GetInstance();
+  s1.PrintAddress();
+
+  Singleton& s2 = Singleton::GetInstance();
+  s2.PrintAddress();
+
+  return 0;
+}
